4cdc55a99b7420902abc92fc8511d562f4c9ffb9
Misha Khmara <1927mishakhmara03@gmail.com>
2020-11-30 16:29:54 +0200

    hw-8

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,15 @@
-// (() => {
-//   const menuBtnRef = document.querySelector("[data-menu-button]");
-//   const mobileMenuRef = document.querySelector("[data-menu]");
 
-//   menuBtnRef.addEventListener("click", () => {
-//     const expanded =
-//       menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
 
-//     menuBtnRef.classList.toggle("is-open");
-//     menuBtnRef.setAttribute("aria-expanded", !expanded);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
-//     mobileMenuRef.classList.toggle("is-open");
-//   });
-// })();
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();

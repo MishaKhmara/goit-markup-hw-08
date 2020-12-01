bc84cb2e5e73aa1c6b4b274dcb15b9eb5f2941a5
Misha Khmara <1927mishakhmara03@gmail.com>
2020-12-01 11:22:47 +0200

    hw-8

--- js/menu.js
+++ js/menu.js
@@ -17,15 +17,3 @@
 
 
 
-// $(document).ready(function(){
-// 	// menu click event
-// 	$('.menuBtn').click(function() {
-// 		$(this).toggleClass('act');
-// 			if($(this).hasClass('act')) {
-// 				$('.mainMenu').addClass('act');
-// 			}
-// 			else {
-// 				$('.mainMenu').removeClass('act');
-// 			}
-// 	});
-// });
